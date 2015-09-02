ae360ddff0a0372e5412885d58a7f809915e4d99
Josh Bradley <jbradley@arcticlcc.org>
2015-09-01 16:53:09 -0800

    Addd methods to entities.

--- src/MetaCat/Entity/Product.php
+++ src/MetaCat/Entity/Product.php
@@ -10,4 +10,124 @@ class Product {
 
     private $project;
     private $projectid;
+
+    /**
+     * Get productid
+     *
+     * @return guid
+     */
+    public function getProductid() {
+        return $this->productid;
+    }
+
+    /**
+     * Set projectid
+     *
+     * @param guid $projectid
+     *
+     * @return Product
+     */
+    public function setProjectid($projectid) {
+        $this->projectid = $projectid;
+
+        return $this;
+    }
+
+    /**
+     * Get projectid
+     *
+     * @return guid
+     */
+    public function getProjectid() {
+        return $this->projectid;
+    }
+
+    /**
+     * Set json
+     *
+     * @param jsonb $json
+     *
+     * @return Product
+     */
+    public function setJson($json) {
+        $this->json = $json;
+
+        return $this;
+    }
+
+    /**
+     * Get json
+     *
+     * @return jsonb
+     */
+    public function getJson() {
+        return $this->json;
+    }
+
+    /**
+     * Set xml
+     *
+     * @param string $xml
+     *
+     * @return Product
+     */
+    public function setXml($xml) {
+        $this->xml = $xml;
+
+        return $this;
+    }
+
+    /**
+     * Get xml
+     *
+     * @return string
+     */
+    public function getXml() {
+        return $this->xml;
+    }
+
+    /**
+     * Set html
+     *
+     * @param string $html
+     *
+     * @return Product
+     */
+    public function setHtml($html) {
+        $this->html = $html;
+
+        return $this;
+    }
+
+    /**
+     * Get html
+     *
+     * @return string
+     */
+    public function getHtml() {
+        return $this->html;
+    }
+
+    /**
+     * Set project
+     *
+     * @param \MetaCat\Entity\Project $project
+     *
+     * @return Product
+     */
+    public function setProject(\MetaCat\Entity\Project $project = null) {
+        $this->project = $project;
+
+        return $this;
+    }
+
+    /**
+     * Get project
+     *
+     * @return \MetaCat\Entity\Project
+     */
+    public function getProject() {
+        return $this->project;
+    }
+
 }
--- src/MetaCat/Entity/Project.php
+++ src/MetaCat/Entity/Project.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace MetaCat\Entity;
 
 class Project {
@@ -9,4 +8,117 @@ class Project {
     protected $xml;
 
     private $products;
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        $this->products = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get projectid
+     *
+     * @return guid
+     */
+    public function getProjectid() {
+        return $this->projectid;
+    }
+
+    /**
+     * Set json
+     *
+     * @param jsonb $json
+     *
+     * @return Project
+     */
+    public function setJson($json) {
+        $this->json = $json;
+
+        return $this;
+    }
+
+    /**
+     * Get json
+     *
+     * @return jsonb
+     */
+    public function getJson() {
+        return $this->json;
+    }
+
+    /**
+     * Set xml
+     *
+     * @param string $xml
+     *
+     * @return Project
+     */
+    public function setXml($xml) {
+        $this->xml = $xml;
+
+        return $this;
+    }
+
+    /**
+     * Get xml
+     *
+     * @return string
+     */
+    public function getXml() {
+        return $this->xml;
+    }
+
+    /**
+     * Set html
+     *
+     * @param string $html
+     *
+     * @return Project
+     */
+    public function setHtml($html) {
+        $this->html = $html;
+
+        return $this;
+    }
+
+    /**
+     * Get html
+     *
+     * @return string
+     */
+    public function getHtml() {
+        return $this->html;
+    }
+
+    /**
+     * Add product
+     *
+     * @param \MetaCat\Entity\Product $product
+     *
+     * @return Project
+     */
+    public function addProduct(\MetaCat\Entity\Product $product) {
+        $this->products[] = $product;
+
+        return $this;
+    }
+
+    /**
+     * Remove product
+     *
+     * @param \MetaCat\Entity\Product $product
+     */
+    public function removeProduct(\MetaCat\Entity\Product $product) {
+        $this->products->removeElement($product);
+    }
+
+    /**
+     * Get products
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getProducts() {
+        return $this->products;
+    }
+
 }
