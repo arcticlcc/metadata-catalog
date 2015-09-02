bc3db2adc46b81abbdd734f8e9678b573a9f03bc
Josh Bradley <jbradley@arcticlcc.org>
2015-09-01 16:51:50 -0800

    Add DebugServiceProvider, change profiler mount point.

--- config/dev.php
+++ config/dev.php
@@ -2,6 +2,7 @@
 
 use Silex\Provider\MonologServiceProvider;
 use Silex\Provider\WebProfilerServiceProvider;
+use Silex\Provider\DebugServiceProvider;
 
 // include the prod configuration
 require __DIR__.'/prod.php';
@@ -15,4 +16,10 @@ $app->register(new MonologServiceProvider(), array(
 
 $app->register(new WebProfilerServiceProvider(), array(
     'profiler.cache_dir' => __DIR__.'/../var/cache/profiler',
+    'profiler.mount_prefix' => '/_silex/profiler',
+));
+
+$app->register(new DebugServiceProvider(), array(
+    'debug.max_items' => 250, // this is the default
+    'debug.max_string_length' => -1, // this is the default
 ));
