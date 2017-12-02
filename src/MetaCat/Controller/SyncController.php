0aa08acb4352f3fd31da9753ca038752e5a1288c
Josh Bradley <jbradley@arcticlcc.org>
2017-12-01 18:24:53 -0900

    Handle multi-part uploads
    Add logic to validate multi-part etags

--- src/MetaCat/Controller/SyncController.php
+++ src/MetaCat/Controller/SyncController.php
@@ -15,6 +15,80 @@ use Aws\S3\Exception\S3Exception;
 
 class SyncController implements ControllerProviderInterface
 {
+      private function calculate_etag($filename, $chunksize, $expected = false) {
+        /*
+        DESCRIPTION:
+        - calculate Amazon AWS ETag used on the S3 service
+        INPUT:
+        - $filename : path to file to check
+        - $chunksize : chunk size in Megabytes
+        - $expected : verify calculated etag against this specified etag and return true or false instead
+            - if you make chunksize negative (eg. -8 instead of 8) the function will guess the chunksize by checking all possible sizes given the number of parts mentioned in $expected
+        OUTPUT:
+        - ETag (string)
+        - or boolean true|false if $expected is set
+        */
+        if ($chunksize < 0) {
+            $do_guess = true;
+            $chunksize = 0 - $chunksize;
+        } else {
+            $do_guess = false;
+        }
+
+        $chunkbytes = $chunksize*1024*1024;
+        $filesize = filesize($filename);
+        if ($filesize < $chunkbytes && (!$expected || !preg_match("/^\\w{32}-\\w+$/", $expected))) {
+            $return = md5_file($filename);
+            if ($expected) {
+                $expected = strtolower($expected);
+                return ($expected === $return ? true : false);
+            } else {
+                return $return;
+            }
+        } else {
+            $md5s = array();
+            $handle = fopen($filename, 'rb');
+            if ($handle === false) {
+                return false;
+            }
+            while (!feof($handle)) {
+                $buffer = fread($handle, $chunkbytes);
+                $md5s[] = md5($buffer);
+                unset($buffer);
+            }
+            fclose($handle);
+
+            $concat = '';
+            foreach ($md5s as $indx => $md5) {
+                $concat .= hex2bin($md5);
+            }
+            $return = md5($concat) .'-'. count($md5s);
+            if ($expected) {
+                $expected = strtolower($expected);
+                $matches = ($expected === $return ? true : false);
+                if ($matches || $do_guess == false || strlen($expected) == 32) {
+                    return $matches;
+                } else {
+                    // Guess the chunk size
+                    preg_match("/-(\\d+)$/", $expected, $match);
+                    $parts = $match[1];
+                    $min_chunk = ceil($filesize / $parts /1024/1024);
+                    $max_chunk =  floor($filesize / ($parts-1) /1024/1024);
+                    $found_match = false;
+                    for ($i = $min_chunk; $i <= $max_chunk; $i++) {
+                        if ($this->calculate_etag($filename, $i) === $expected) {
+                            $found_match = true;
+                            break;
+                        }
+                    }
+                    return $found_match;
+                }
+            } else {
+                return $return;
+            }
+        }
+    }
+
     public function connect(Application $app)
     {
         $controllers = $app['controllers_factory'];
@@ -73,7 +147,7 @@ class SyncController implements ControllerProviderInterface
                     if (file_exists($temp)) {
                         //Validate md5(skip for multi-part)
                         $s3Etag = str_replace('"', '', $result['ETag']);
-                        if (md5_file($temp) !== $s3Etag || strpos($s3Etag, '-') !== false) {
+                        if (md5_file($temp) !== $s3Etag && !$this->calculate_etag($temp, -8, $s3Etag)) {
                             throw new \Exception('S3 exception. md5 validation failed.');
                         }
                         //unzip the contents
